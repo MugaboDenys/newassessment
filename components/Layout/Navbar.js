6623704920f3573b45416adf2718fe27ead85cf9
Mugabodenys <mugabodan2020@gmail.com>
2023-01-13 15:59:47 +0200

    fixed menu

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,32 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/logo-inverted-en.svg";
+import logo1 from "../../public/logo-inverted-en2.svg";
+import search1 from "../../public/search1.svg";
+import search2 from "../../public/search2.svg";
+import cross from "../../public/cross.svg";
+import menu2 from "../../public/menu2.svg";
 import lan from "../../public/lan.png";
 import menu from "../../public/menu.png";
 import search from "../../public/search.png";
-
+import { useState } from "react";
 
 const Navbar = () => {
+  let [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
   return (
-    <div className="bg-white sticky top-0 z-10">
-      <div className="max-w-[960px] mx-auto flex justify-between py-10 px-5 md:px-0 ">
+    <div className={`${open ? `bg-darkish` : `bg-white` } duration-300  sticky top-0 z-10`}>
+      <div className="max-w-[960px] mx-auto flex justify-between py-10 px-5 md:px-0 relative">
         <Link href={"/"}>
-          <Image src={logo} alt="" className="w-full h-14 md:h-20" />
+          <Image src={open ? logo1: logo} alt="" className="w-full h-14 md:h-20" />
         </Link>
         <div className="flex gap-x-2">
           <div className="hidden md:flex gap-x-2">
             <Link href={"/"}>
-              <Image alt="" src={lan} width={45} height={45} />
+              <Image alt="" src={lan} width={45} height={45} className={`${open ? "hidden":"block"}`} />
             </Link>
             <Link href={"/"}>
-              <Image alt="" src={search} width={45} height={45} />
+              <Image className="pt-2" alt="" src={open ? search1 : search2} width={35} height={35} />
             </Link>
           </div>
           <Link href={"/"}>
-            <Image alt="" src={menu} width={45} height={45} />
+            <Image
+              onClick={handleOpen}
+              alt=""
+              src={open ? cross : menu2}
+              width={45}
+              height={45}
+            />
           </Link>
         </div>
       </div>
+      {open ? <div className="h-screen w-full absolute  bg-opacity-90 bg-darkish">
+        <div className="max-w-[960px] mx-auto mt-60 text-white ">
+        <Link href={'/programs'} onClick={handleOpen} className="block font-bold text-4xl py-3 px-5 duration-300 hover:text-darkish hover:bg-paleblue">All Study pro</Link>
+        </div>
+      </div> : null }
     </div>
   );
 };
---
+++ pages/programs/index.js
@@ -0,0 +1,22 @@
+const programs = () => {
+  return (
+    <div className="h-screen bg-darkblue text-white">
+      <h3 className="text-3xl font-bold">Lorem, ipsum.</h3>
+      <p className="mt-52 text-xs">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aperiam
+        harum totam. Tenetur reiciendis ipsam, ipsa quasi dolor animi magni
+        incidunt inventore asperiores vel est nihil totam ullam iusto itaque
+        fuga doloremque sint eum! Ad accusantium nihil ut quod similique natus
+        est repudiandae eius iusto adipisci ea minima mollitia rerum tempora
+        vero illum quam, ratione perspiciatis, accusamus veritatis labore omnis
+        explicabo error et! Commodi omnis rerum reprehenderit, ad ipsam
+        voluptates optio laborum quas, modi mollitia nihil molestias at debitis,
+        hic magni nisi similique doloremque culpa repellat! Tempore magni
+        accusamus reprehenderit tenetur, vel cum mollitia repellendus dolore!
+        Eaque id cumque tenetur!
+      </p>
+    </div>
+  );
+};
+
+export default programs;
